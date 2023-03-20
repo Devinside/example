638e080fc66524fba8c28af759f90e2b9ec3e5b2
Devinside <ibesca@gmail.com>
2023-03-21 07:19:17 +0900

    Update container_check.sh

--- container_check.sh
+++ container_check.sh
@@ -5,8 +5,10 @@ if [ -n "$NGINX_CONTAINER_ID" ];
 		echo "nginx container exist!" 
 		docker stop $NGINX_CONTAINER_ID 
 		docker rm $NGINX_CONTAINER_ID 
-		docker run -d -p 80:80 --name nginx nginx:test 
+		#docker run -d -p 80:80 --name nginx nginx:test 
+		docker-compose up -d
 	else 
 		echo "nginx container not exist!"
-		docker run -d -p 80:80 --name nginx nginx:test
+		#docker run -d -p 80:80 --name nginx nginx:test
+		docker-compose up -d
 fi
